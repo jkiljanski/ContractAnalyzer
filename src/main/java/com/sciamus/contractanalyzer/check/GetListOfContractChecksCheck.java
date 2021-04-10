7971bac3b1f816f815847e6b0533ee8aa34e81d9
Wojtek Bączkowski <wojciech.baczkowski@sciamus.com>
2021-04-10 09:59:57 +0200

    Changed log level and get second check working

--- src/main/java/com/sciamus/contractanalyzer/check/GetListOfContractChecksCheck.java
+++ src/main/java/com/sciamus/contractanalyzer/check/GetListOfContractChecksCheck.java
@@ -4,10 +4,13 @@ import com.sciamus.contractanalyzer.control.GetListOfContractChecksCheckClient;
 import com.sciamus.contractanalyzer.reporting.ReportResults;
 import com.sciamus.contractanalyzer.reporting.TestReport;
 import feign.Feign;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import feign.codec.StringDecoder;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
-import java.util.List;
 
 @Component
 public class GetListOfContractChecksCheck implements RestContractCheck {
@@ -23,16 +26,19 @@ public class GetListOfContractChecksCheck implements RestContractCheck {
 //    }
 
     @Override
+//TODO: pytanie: nie dość, że wymagamy od sprawdzanego serwera wystawienia konkrentego endpointa, to jeszcze definiujemy konkretą metodę??
+
     public TestReport run(URL url) {
 
         GetListOfContractChecksCheckClient testClient = Feign.builder()
+
 //                .client(client)
                 .target(GetListOfContractChecksCheckClient.class, url.toString());
-
 
         if (testClient.getListOfChecks().isEmpty()) {
             return new TestReport(ReportResults.FAILED, "Test failed");
         }
+        System.out.println(testClient.getListOfChecks());
         return new TestReport(ReportResults.PASSED, "Test passed");
     }
 
--- src/main/java/com/sciamus/contractanalyzer/control/GetListOfContractChecksCheckClient.java
+++ src/main/java/com/sciamus/contractanalyzer/control/GetListOfContractChecksCheckClient.java
@@ -1,15 +1,20 @@
 package com.sciamus.contractanalyzer.control;
 
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @FeignClient(value = "GetListOfContractChecksCheckClient")
 public interface GetListOfContractChecksCheckClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/restContractChecks")
-    List<String> getListOfChecks();
+//    @GetMapping( "/restContractChecks")
+    @RequestLine(value="GET /restContractChecks")
+    //TODO: czy tak jest okej - czy nie potrzeba bardziej skomplikowanego obiektu response???
+    String getListOfChecks();
 
 }
