8604007ceb4a183aecadc3090cc4b05279e4f6e1
Anastasiya4567 <302439@pw.edu.pl>
2021-05-24 14:15:03 +0200

    Swagger on http://localhost:8080/swagger-ui.html added

--- src/main/java/com/sciamus/contractanalyzer/DemoApplication.java
+++ src/main/java/com/sciamus/contractanalyzer/DemoApplication.java
@@ -7,11 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
 @EnableFeignClients
 @EnableMongoRepositories(basePackages = "com.sciamus.contractanalyzer.reporting")
-
+@EnableSwagger2
 //(basePackages = "com.sciamus.contractanalyzer.reporting")
 public class DemoApplication {
 
@@ -22,5 +26,11 @@ public class DemoApplication {
     @Bean
     VavrModule vavrModule() {
         return new VavrModule();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.sciamus.contractanalyzer")).build();
     }
 }
