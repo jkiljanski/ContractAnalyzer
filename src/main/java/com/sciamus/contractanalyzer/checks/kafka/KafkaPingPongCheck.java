6b404bdf7e520c5ff4d4120a348490b587f26f2e
Wojtek Bączkowski <wojciech.baczkowski@sciamus.com>
2021-05-26 16:53:00 +0200

    simple kafka connection test working

--- src/main/java/com/sciamus/contractanalyzer/checks/kafka/KafkaPingPongCheck.java
+++ src/main/java/com/sciamus/contractanalyzer/checks/kafka/KafkaPingPongCheck.java
@@ -1,20 +1,19 @@
 package com.sciamus.contractanalyzer.checks.kafka;
 
-import ch.qos.logback.core.BasicStatusManager;
-import ch.qos.logback.core.helpers.CyclicBuffer;
 import com.sciamus.contractanalyzer.checks.kafka.clients.KafkaPingProducer;
 import com.sciamus.contractanalyzer.checks.kafka.clients.KafkaPongConsumer;
 import com.sciamus.contractanalyzer.reporting.checks.CheckReport;
 import com.sciamus.contractanalyzer.reporting.checks.ReportResults;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -31,7 +30,7 @@ public class KafkaPingPongCheck implements KafkaContractCheck {
 
 
     private final KafkaPongConsumer kafkaPongConsumer;
-    private BlockingQueue<String> consumedMessages =new ArrayBlockingQueue<>(100);
+    private BlockingQueue<String> consumedMessages = new ArrayBlockingQueue<>(100);
 
 
     public KafkaPingPongCheck(KafkaPingProducer kafkaPingProducer, KafkaPongConsumer kafkaPongConsumer) {
@@ -46,7 +45,7 @@ public class KafkaPingPongCheck implements KafkaContractCheck {
         //adres brokera
 
         Map<String, Object> consumerConfig = Collections.unmodifiableMap(Map.of(
-                BOOTSTRAP_SERVERS_CONFIG, host + port,
+                BOOTSTRAP_SERVERS_CONFIG, host+":" + port,
                 GROUP_ID_CONFIG, "1"
         ));
 
@@ -55,7 +54,6 @@ public class KafkaPingPongCheck implements KafkaContractCheck {
                         consumerConfig,
                         new StringDeserializer(),
                         new StringDeserializer());
-
 
 
         ContainerProperties containerProperties = new ContainerProperties(incomingTopic);
@@ -75,11 +73,12 @@ public class KafkaPingPongCheck implements KafkaContractCheck {
         } catch (InterruptedException e) {
             e.printStackTrace();
             System.out.println("failed but active");
-            return new CheckReport("12", ReportResults.FAILED,"xxx",new Date(), "kafka");
+            return new CheckReport("12", ReportResults.FAILED, "xxx", new Date(), "kafka");
         }
 
         System.out.println("passed and active");
-        return new CheckReport("13",ReportResults.FAILED,"sss",new Date(), "kafka");
+        System.out.println(consumedMessages);
+        return new CheckReport("13", ReportResults.FAILED, "sss", new Date(), "kafka");
 
     }
 
--- src/main/java/com/sciamus/contractanalyzer/control/KafkaCheckController.java
+++ src/main/java/com/sciamus/contractanalyzer/control/KafkaCheckController.java
@@ -1,11 +1,10 @@
 package com.sciamus.contractanalyzer.control;
 
-import com.sciamus.contractanalyzer.checks.kafka.KafkaContractCheck;
 import com.sciamus.contractanalyzer.checks.kafka.KafkaContractService;
 import com.sciamus.contractanalyzer.reporting.checks.CheckReport;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,17 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class KafkaCheckController {
 
-
-    KafkaContractService kafkaContractService;
-
+    private final KafkaContractService kafkaContractService;
 
 
-//add parameters
-    @PostMapping("/kafkaContractCheck/pingpong?firstTopic={firstTopic}&secondTopic={secondTopic}&host={host}&port={port}")
-    CheckReport runKafka(@Param("firstTopic") String firstTopic, @Param("secondTopic") String secondTopic, @Param("host") String host, @Param("port") String port) {
+    public KafkaCheckController(KafkaContractService kafkaContractService) {
+        this.kafkaContractService = kafkaContractService;
+    }
 
-    return kafkaContractService.runKafkaCheck(firstTopic, secondTopic, host, port);
+    //add parameters
+    @GetMapping("/kafkaContractCheck/pingpong")
+    CheckReport runKafka(@RequestParam("firstTopic") String firstTopic, @RequestParam("secondTopic") String secondTopic, @RequestParam("host") String host, @RequestParam("port") String port) {
+
+        return kafkaContractService.runKafkaCheck(firstTopic, secondTopic, host, port);
 
     }
 
 }
+//?firstTopic={firstTopic}&secondTopic={secondTopic}&host={host}&port={port}
