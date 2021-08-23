e82ce10fa2f58bdd8da650d9a1f0fbbff9cb345a
Wojtek Bączkowski <wojciech.baczkowski@sciamus.com>
2021-08-23 15:41:59 +0200

    QDSL still stuck on Vavr list

--- src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/MongoReportsRepository.java
+++ src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/MongoReportsRepository.java
@@ -5,28 +5,7 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface MongoReportsRepository extends MongoRepository<ReportDocument, String>, QuerydslPredicateExecutor<ReportDocument> {
 
-//    @Query("{$or:[{ $expr: { $eq: ['?0', 'null'] }},{result: ?0}]}" +
-//            "{$or:[{ $expr: { $eq: ['?1', 'null'] }},{content:{$regex : ?1}}]}" +
-//            "{$or:[{ $expr: { $eq: ['?2', 'null'] }},{ $expr: { $eq: ['?3', 'null'] }},{timestamp : {$gte : ?2, $lt : ?3 }]}" +
-//            "{$or:[{ $expr: { $eq: ['?4', 'null'] }}, { name: ?4 } ]}" +
-//            "{$or:[{ $expr: { $eq: ['?5', 'null'] }}, { userName: ?5 } ]}")
-
-//    @Query("{ $or : [ { $expr: { $eq: ['?0', 'null'] } , { result : ?0 } ] }")
-
-    //THIS SHIT WORKS:
-//    @Query("{ result : ?0 }")
-//    @Query("{ $or : [ { $expr: { $eq: ['?0', 'null'] }} , { result : ?0 } ] }")
-
-
-//    @Query("{$or:[{ $expr: { $eq: ['?1', 'null'] }},{content:{$regex : ?1}}]}")
-
-
-
-//    Page<ReportDocument> findAll(String result, String reportBody, String timestampFrom, String timestampTo, String nameOfCheck, String userName, Pageable pageable);
-
 
 
 
 }
-
-//{ $and: [
--- src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/ReportDocument.java
+++ src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/ReportDocument.java
@@ -17,7 +17,6 @@ import java.time.LocalDateTime;
 public class ReportDocument {
 
 
-
     @Id
     public String id;
     @Field("result")
