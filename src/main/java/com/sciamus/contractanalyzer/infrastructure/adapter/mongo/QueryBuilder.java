42d28c6fb60ea6a2ed0371049050c68a22fcfbc6
Wojtek Bączkowski <wojciech.baczkowski@sciamus.com>
2021-08-25 11:27:34 +0200

    before moving to branch 76

--- src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/QueryBuilder.java
+++ src/main/java/com/sciamus/contractanalyzer/infrastructure/adapter/mongo/QueryBuilder.java
@@ -1,6 +1,7 @@
 package com.sciamus.contractanalyzer.infrastructure.adapter.mongo;
 
 import com.sciamus.contractanalyzer.application.ReportFilterParameters;
+import io.vavr.collection.List;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
@@ -11,7 +12,12 @@ public class QueryBuilder {
     Query buildQuery(ReportFilterParameters reportFilterParameters) {
 
         Query query = new Query();
+
+        List<Criteria> criteria = List.empty();
+
         Criteria andCriteria = new Criteria();
+
+
 
         query.addCriteria(Criteria.where("result").regex(reportFilterParameters.result.getOrElse("")));
         query.addCriteria(Criteria.where("content").regex(reportFilterParameters.reportBody.getOrElse("")));
@@ -19,8 +25,10 @@ public class QueryBuilder {
         query.addCriteria(Criteria.where("name").regex(reportFilterParameters.nameOfCheck.getOrElse("")));
         query.addCriteria(Criteria.where("userName").regex(reportFilterParameters.userName.getOrElse("")));
 
+
         LocalDateTime from = LocalDateTime.parse(reportFilterParameters.timestampFrom.getOrElse("1300-08-24T07:34:09.013"));
         LocalDateTime to = LocalDateTime.parse(reportFilterParameters.timestampTo.getOrElse("3200-08-24T07:34:09.013"));
+
 
         andCriteria.andOperator(Criteria.where("timestamp").gte(from),
                 Criteria.where("timestamp").lt(to));
